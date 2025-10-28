e2ab6562dc216f497978be9b7970c34429aebbec
shijiu168998-cellt <shijiu168998@gmail.com>
2025-10-29 02:46:13 +0800

    Update website 2025/10/29 周三  2:46:13.53

--- game/upload.bat
+++ game/upload.bat
@@ -40,6 +40,10 @@ echo --------------------------------------------
 echo 🔄 同步远程仓库...
 git pull origin main --allow-unrelated-histories
 
+git add .
+git commit -m "fix commit lock error"
+git push -u origin main
+
 REM 推送到远程仓库
 echo --------------------------------------------
 echo 🚀 正在推送到 GitHub...
